2736e7600b499a7ff8d37dd5ba72ca98edc71030
oledibefrancis <somtofrancis380@gmail.com>
2023-02-05 02:10:34 -0600

    Updated and added contoller Tests

--- summativeProjectOne/src/test/java/com/company/summativeProjectOne/controllers/MagicControllerTest.java
+++ summativeProjectOne/src/test/java/com/company/summativeProjectOne/controllers/MagicControllerTest.java
@@ -1,23 +1,21 @@
 package com.company.summativeProjectOne.controllers;
-
+import com.company.summativeProjectOne.models.Answer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(WordController.class)
-public class WordControllerTest {
-
+@WebMvcTest(MagicController.class)
+public class MagicControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,12 +23,23 @@ public class WordControllerTest {
     private ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void shouldReturnQuote() throws Exception{
-        mockMvc.perform(get("/word"))
+    public void shouldReturnAnswer() throws Exception{
+        Answer answer = new Answer(1,"What is your name", "");
+
+        String inputJson = mapper.writeValueAsString(answer);
+
+
+
+        mockMvc.perform(post("/magic")
+                .content(inputJson)
+                .contentType(MediaType.APPLICATION_JSON)
+
+        )
+                .andExpect(status().isCreated())
                 .andDo(print())
-                .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isNotEmpty());
+
     }
 
-    
+
 }
